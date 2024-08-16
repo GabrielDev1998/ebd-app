27e9253c6b519fda4dacac19b79441f4e975da20
Gabriel Souza <dev.gabriel98@gmail.com>
2024-08-16 11:41:02 -0300

    Atualizado em 16-08

---
+++ src/components/pages/aulas/calendar/calendar.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+import styles from './calendar.module.css';
+
+const Calendar = () => {
+  return <div className={styles.containerCalendar}>calendar</div>;
+};
+export default Calendar;
--- src/components/settings/appearance/appearance.tsx
+++ src/components/settings/appearance/appearance.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import styles from '../settings.module.css';
 import { ThemeEBD, themeColors } from '@/providers/theme/theme-context';
-import { Icon } from '@iconify/react/dist/iconify.js';
 
 export type ColorsEBD = {
   hexP: string;
@@ -15,14 +14,12 @@ const { color_1, color_2, color_3, color_4, color_5, color_6 } = themeColors;
 const ColorOption = ({
   hexP,
   hexS,
-  text,
 }: {
   hexP: string;
   hexS: string;
-  text: string;
 } & ColorsEBD) => {
   const { setPropertyDoc } = ThemeEBD();
-  
+
   const handleClickColor = () => {
     // Color primary
     setPropertyDoc({
@@ -65,7 +62,6 @@ const ColorOption = ({
   return (
     <div data-cor={hexP}>
       <span onClick={handleClickColor} style={{ backgroundColor: hexP }}></span>
-      <p>{text}</p>
     </div>
   );
 };
@@ -77,13 +73,12 @@ const Appearance = () => {
         <ColorOption
           hexP={color_1.hexP}
           hexS={color_1.hexS}
-          text="Roxo (padrão)"
         />
-        <ColorOption hexP={color_2.hexP} hexS={color_2.hexS} text="Amarelo" />
-        <ColorOption hexP={color_3.hexP} hexS={color_3.hexS} text="Laranja" />
-        <ColorOption hexP={color_4.hexP} hexS={color_4.hexS} text="Rosa" />
-        <ColorOption hexP={color_5.hexP} hexS={color_5.hexS} text="Verde" />
-        <ColorOption hexP={color_6.hexP} hexS={color_6.hexS} text="Azul" />
+        <ColorOption hexP={color_2.hexP} hexS={color_2.hexS}  />
+        <ColorOption hexP={color_3.hexP} hexS={color_3.hexS}  />
+        <ColorOption hexP={color_4.hexP} hexS={color_4.hexS}  />
+        <ColorOption hexP={color_5.hexP} hexS={color_5.hexS} />
+        <ColorOption hexP={color_6.hexP} hexS={color_6.hexS} />
       </div>
     </div>
   );
